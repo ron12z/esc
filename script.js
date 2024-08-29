7ce013ab5adb5f19ad3bf27545bb0dbcbdd56951
ron12z <aarontalua14@gmail.com>
2024-08-29 19:08:51 +0800

    Working prototype with detailed descriptions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const result = document.querySelector(".result");
 const escalation = document.querySelector("#escalation");
 const options = document.querySelector(".options");
+const amount = document.querySelector("#amount");
 const suspended = document.querySelector("#suspended");
 const name1 = document.querySelector("#name");
 const email = document.querySelector("#email");
@@ -86,6 +87,15 @@ function GenerateText() {
 	let result = "Escalating Case - ";
 	let first_escalation = true;
 
+	if (isChecked(amount)) {
+		if (first_escalation) {
+			result += "Withdrawal amount is greater than $5,000";
+			first_escalation = false;
+		} else {
+			result += ", and withdrawal amount is greater than $5,000";
+		}
+	}
+
 	if (isChecked(suspended)) {
 		if (first_escalation) {
 			result += "Account is suspended";
@@ -98,94 +108,100 @@ function GenerateText() {
 	if (isChecked(name1)) {
 		//Get names
 		if (first_escalation) {
-			result += "Name mismatch";
+			result += "Multiple names in account - (name1), (name2)";
 			first_escalation = false;
 		} else {
-			result += ", and name mismatch";
+			result += ", and has multiple names in account - (name1), (name2)";
 		}
 	}
 
 	if (isChecked(email)) {
 		if (first_escalation) {
-			result += "Email mismatch";
+			result +=
+				"Account is less than 1 month old with email mismatch - (email1)";
 			first_escalation = false;
 		} else {
-			result += ", and email mismatch";
+			result +=
+				", and account is less than 1 month old with email mismatch - (email1)";
 		}
 	}
 
 	if (isChecked(lost)) {
 		if (first_escalation) {
-			result += "Has lost/stolen card";
+			result += "(Card1) has been reported lost or stolen";
 			first_escalation = false;
 		} else {
-			result += ", and has lost/stolen card";
+			result += ", and (Card1) has been reported lost or stolen";
 		}
 	}
 
 	if (isChecked(sa1)) {
 		//Get cards
 		if (first_escalation) {
-			result += "SA1";
+			result +=
+				"Withdrawing to a payment method (card1) used for small deposit with larger deposits made from (card2)";
 			first_escalation = false;
 		} else {
-			result += ", and SA1";
+			result +=
+				", and withdrawing to a payment method (card1) used for small deposit with larger deposits made from (card2)";
 		}
 	}
 
 	if (isChecked(sa2)) {
 		//Get cards
 		if (first_escalation) {
-			result += "SA2";
+			result +=
+				"Customer has attempted to deposit with multiple debit cards in the past 24 hours - (card1), (card2), (card3)";
 			first_escalation = false;
 		} else {
-			result += ", and SA2";
+			result +=
+				", and customer has attempted to deposit with multiple debit cards in the past 24 hours - (card1), (card2), (card3)";
 		}
 	}
 
 	if (isChecked(cashed)) {
 		if (first_escalation) {
-			result += "Cashed out";
+			result += "Cashed out bet before withdrawal";
 			first_escalation = false;
 		} else {
-			result += ", and cashed out";
+			result += ", and cashed out bet before withdrawal";
 		}
 	}
 
 	if (isChecked(short)) {
 		if (first_escalation) {
-			result += "Has short odds";
+			result += "Has short odds bet placement";
 			first_escalation = false;
 		} else {
-			result += ", and short odds";
+			result += ", and has short odds bet placement";
 		}
 	}
 
 	if (isChecked(user)) {
 		//Get number of related users
 		if (first_escalation) {
-			result += "Has 4 related users";
+			result += "Account is sharing a device with (x) related users";
 			first_escalation = false;
 		} else {
-			result += ", and 4 related users";
+			result += ", and account is sharing a device with (x) related users";
 		}
 	}
 
 	if (isChecked(outside)) {
 		if (first_escalation) {
-			result += "Client is outside US";
+			result += "Account is Geo-locating outside US";
 			first_escalation = false;
 		} else {
-			result += ", and outside US";
+			result += ", and account is Geo-locating outside US";
 		}
 	}
 
 	if (isChecked(newloc)) {
 		if (first_escalation) {
-			result += "New device in a new location";
+			result += "Account is on a new device in a new location";
 			first_escalation = false;
 		} else {
-			result += ", and new device in a new location";
+			result += ", account is on a new device in a new location";
 		}
 	}
 
